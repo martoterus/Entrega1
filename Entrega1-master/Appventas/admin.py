ec7872f6847be84e964b56e4294a877eddd63892
martoterus <108109012+martoterus@users.noreply.github.com>
2022-07-27 15:28:35 -0300

    Delete Entrega1-master directory

--- Entrega1-master/Appventas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from Appventas.models import bicicletas, indumentaria, repuestos
-
-# Register your models here.
-
-admin.site.register(bicicletas)
-admin.site.register(repuestos)
-admin.site.register(indumentaria)
